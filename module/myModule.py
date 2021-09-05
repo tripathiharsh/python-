af2a11361c3dfcdd37fc40c54f47797f9397d080
tripathiharsh <77981237+tripathiharsh@users.noreply.github.com>
2021-09-05 19:30:13 +0530

    Add files via upload

---
+++ module/myModule.py
@@ -0,0 +1,15 @@
+
+#I am going to create module that is going to print about my self
+rupesh = {
+  "name": "rupesh",
+  "last name":"tripathi",
+  "age": 14,
+  "country": "India",
+  "city":"allahabad",
+  "state":"uttar-pradesh",
+  "father-name":"Ripu Sudan Tripathi",
+  "mother-name":"Anita tripathi",
+  "school":"Gurukul Montesori school",
+  "sister name":"Nikita tripathi",
+  "DOB":"2nd august 2006"
+}
---
+++ module/test_module.py
@@ -0,0 +1,6 @@
+import myModule
+
+a = myModule.rupesh['father-name']
+print(a)
+n = myModule.rupesh['sister name']
+print(n)
